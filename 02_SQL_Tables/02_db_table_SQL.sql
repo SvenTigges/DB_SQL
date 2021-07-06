12af7b6a1d1be49aaa7ae97950a225d4928e2d1c
SvenTigges <sven.tigges@web.de>
2021-07-06 08:19:03 +0000

    add test Tabelle

--- 02_SQL_Tables/02_db_table_SQL.sql
+++ 02_SQL_Tables/02_db_table_SQL.sql
@@ -1,5 +1,37 @@
 /* ----Strukturen-----*/
 
 /* Datenbanken auf Server anzeigen */
-SHOW DATABASES; 
+#SHOW DATABASES; 
 
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* Datenbank erzeugen, falls nicht vorhanden */
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* Datenbank auswaehlen */
+USE boo;
+
+/* Tabelle anlegen, falls noch nicht vorhanden */ 
+CREATE TABLE IF NOT EXISTS test 
+(
+    name VARCHAR(20),
+    age INT 
+); 
+
+
+/* Struktur der Tabelle anzeigen*/
+DESCRIBE test; 
+
+/* ------ Daten -----*/
+INSERT INTO test (name,age) VALUES("Grizabella",29);
+INSERT INTO test (age,name) VALUES(35,"Alonzo");
+INSERT INTO test VALUES();
+
+/* Tabelleninhalt anzeigen */
+SELECT * FROM test;
+
+/* Tabelle test löschen, falls vorhaben */
+DROP TABLE IF EXISTS test; 
+
+SHOW TABLES;
