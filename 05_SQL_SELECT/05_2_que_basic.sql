b951340b25f800be93e1f115056b62af411ed0b1
SvenTigges <sven.tigges@web.de>
2021-07-08 09:13:18 +0000

    ++SELECT_++CONCAT

--- 05_SQL_SELECT/05_2_que_basic.sql
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -12,6 +12,7 @@ Ausgabe beschränken
 #SELECT * FROM boo.ccc_list;
 
 -- Begrenzung: 5 Zeilen ab 0
+/*
 SELECT
     * 
 FROM ccc_list
@@ -19,3 +20,42 @@ FROM ccc_list
 #LIMIT 200,10 # 10 Zeilen ab 200 | nach ,wird hoch gezählt 
 LIMIT 100,50
 ;
+*/
+
+-- Abfrage eines Feldes oder einer Feld-Komni (rwos 1-10)
+/*
+SELECT
+    ticker,
+    c_name,
+    industry
+FROM 
+    ccc_list
+limit 5;
+*/
+
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+/*
+SELECT
+    Ticker AS "SYM",
+    c_name AS "Unternehmen",
+    price AS "Kurs in $"
+FROM ccc_list
+LIMIT 10
+; 
+*/
+
+-- Kombination aus mehreren Feldern in neues Feld
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT
+    ticker AS SYM,
+    c_name AS "Unternehmen",
+    CONCAT(sector," | ", industry) AS "Operations"
+FROM ccc_list
+LIMIT 5;
+    
+
+
+
+
+
+
