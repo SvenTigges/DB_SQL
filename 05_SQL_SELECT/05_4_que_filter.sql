f947494338a4e459d301514b063d5ab7a1c0568a
SvenTigges <sven.tigges@web.de>
2021-07-09 07:22:30 +0000

    add_AND_OR_NOT

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -1,34 +1,76 @@
-/* QUERIES filtern mir WHERE */
+/* QUERIES Filtern mit WHERE */
 
 USE boo;
 
 -- Index
-
 /*
-    Eingrenzen/filtern WHERE 
-    Eingrenzen/filtern WHERE
-    Eingrenzen/filtern WHERE
-    Eingrenzen/filtern WHERE
+    Eingrenzen/Filtern WHERE & AND/OR etc.
+    Eingrenzen/Filtern WHERE & LIKE + Parameter
+    Eingrenzen/Filtern WHERE & RegEx
+    Eingrenzen/Filtern WHERE & IN / NOT IN
+    Eingrenzen/Filtern WHERE & BETWEEN / NOT BETWEEN
 */
 
--- Eingrenzen/Filtern WHERE & AND/OR etc.
+
+--  Eingrenzen/Filtern WHERE & AND/OR etc.
+/*
 SELECT
     ticker AS "SYMBOL",
     c_name "Unternehmen",
-    CONCAT(sector," | ",industry) "Operations",
+    CONCAT(sector, " | ", industry) "Operations",
     price "Kurs in $",
     payouts "Zahlungen p.a.",
-    dividend "Dividende"
+    dividend "Dividende" 
 FROM ccc_list
--- Einzeldaten / Stirngs
+-- Einzeldaten / Strings
 #WHERE sector = "Communication Services"
-#ORDER BY price ASC #beginnend mit der günstigsen Aktie
-
+#WHERE industry = "Media"
+-- Kombination durch AND
+#WHERE sector = "Communication Services" AND industry = "Entertainment"
+#WHERE industry = "Entertainment" AND payouts = 4
+-- Kombination durch AND / OR
+#WHERE sector = "Communication Services" AND (industry = "Entertainment" OR industry = "Media")
 -- Kombination durch AND / NOT
-WHERE sector = "Communication Services" AND NOT industry =  "media"
+WHERE sector = "Communication Services" AND NOT industry = "Media"
+#ORDER BY price ASC #beginnend mit günstigster Aktie 
 ORDER BY industry ASC
-
 LIMIT 20;
+*/
+
+-- Eingrenzen/Filtern WHERE & LIKE + Parameter
+SELECT
+    ticker "SYMBOL",
+    c_name "Unternehmen",
+    industry "Branche"
+FROM ccc_list
+
+-- scharfe Suche nach Strings
+#WHERE industry = "Media"
+
+-- unschärfere Suche --
+-- Branchenname beginnt mit ... , dahinter beliebige Chars
+#WHERE industry LIKE "Air%"
+-- Branchenname endet mit ... , davor beliebige Chars
+#WHERE industry LIKE "%ment"
+-- Branchenname enthaelt ...
+#WHERE industry LIKE "%ood%"
+
+-- Branchenname endet/beginnt mit ... , danach/davor/inmitten genau _ Char
+#WHERE industry LIKE "__dia"
+#WHERE industry LIKE "Med__"
+#WHERE industry LIKE "Me__a"
+#WHERE industry LIKE "M_d_a"
+#WHERE industry LIKE "_ir%"
+#WHERE industry LIKE "_ood%"
+
+#WHERE industry LIKE "%ment" 
+#WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" 
+WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" AND industry NOT LIKE "%tain%"
+
+ORDER BY industry ASC
+LIMIT 40;
+    
+    
 
 
 
