75aadc995fc225a742e0ae8abac2a916e6de2851
SvenTigges <sven.tigges@web.de>
2021-07-09 08:17:34 +0000

    add_Between

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -38,15 +38,14 @@ LIMIT 20;
 */
 
 -- Eingrenzen/Filtern WHERE & LIKE + Parameter
+/*
 SELECT
     ticker "SYMBOL",
     c_name "Unternehmen",
     industry "Branche"
 FROM ccc_list
-
 -- scharfe Suche nach Strings
 #WHERE industry = "Media"
-
 -- unschärfere Suche --
 -- Branchenname beginnt mit ... , dahinter beliebige Chars
 #WHERE industry LIKE "Air%"
@@ -54,7 +53,6 @@ FROM ccc_list
 #WHERE industry LIKE "%ment"
 -- Branchenname enthaelt ...
 #WHERE industry LIKE "%ood%"
-/*
 -- Branchenname endet/beginnt mit ... , danach/davor/inmitten genau _ Char
 #WHERE industry LIKE "__dia"
 #WHERE industry LIKE "Med__"
@@ -62,27 +60,49 @@ FROM ccc_list
 #WHERE industry LIKE "M_d_a"
 #WHERE industry LIKE "_ir%"
 #WHERE industry LIKE "_ood%"
-
 #WHERE industry LIKE "%ment" 
 #WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" 
 WHERE industry LIKE "%ment" AND industry NOT LIKE "%ipment" AND industry NOT LIKE "%tain%"
-
 ORDER BY industry ASC
 LIMIT 40;
 */
 
+-- Eingrenzen/Filtern WHERE & RegEx
 /*
 SELECT
-    c_name "Unternehmen"
+	c_name "Unternehmen"
 FROM ccc_list
 WHERE c_name RLIKE "^[AZ]"
 ORDER BY c_name;
-*/    
-    
+*/
 
+--  Eingrenzen/Filtern WHERE & IN / NOT IN (Suchsets)
+/*
+SELECT 
+	ticker "SYM",
+	c_name "Unternehmen",
+	sector "Sektor",
+	industry "Branche"
+FROM ccc_list 
+#WHERE sector = "Financials"
+#WHERE industry IN ("Beverages","Banks") #OR ...
+WHERE sector = "Financials" AND industry NOT IN ("Banks","Insurance")
+ORDER BY industry DESC;
+*/
 
-
-
+-- Eingrenzen/Filtern WHERE & BETWEEN / NOT BETWEEN
+SELECT 
+	ticker "SYM",
+	c_name "Unternehmen",
+	sector "Sektor",
+	industry "Branche",
+	price "Aktienpreis in $"
+FROM ccc_list 
+#WHERE sector = "Financials" AND price > 30.0 
+WHERE sector = "Financials" AND (price BETWEEN 30.0 AND 50.0)
+#WHERE sector = "Financials" AND NOT (price BETWEEN 20.0 AND 250.0)
+ORDER BY price DESC
+LIMIT 100;
 
 
 
